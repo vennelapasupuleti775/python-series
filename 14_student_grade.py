68d5574be3c0e946214ae13a1a18c590bb8e84bf
vennelapasupuleti775 <vennelapasupuleti08@gmail.com>
2026-07-10 05:40:00 +0530

    Create 14_student_grade.py

---
+++ 14_student_grade.py
@@ -0,0 +1,11 @@
+marks=int(input("enter the marks:")
+if marks>=90:
+          print("grade A")
+elif marks>=75:
+          print("grade B")
+elif marks>=60:
+          print("grade C")
+elif marks>=45:
+          print("grade D")
+else:
+          print("Fail")
