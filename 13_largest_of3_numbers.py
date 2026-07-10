038ced345c54e3c3be0a40de0f580e2a27ef5a56
vennelapasupuleti775 <vennelapasupuleti08@gmail.com>
2026-07-10 05:32:38 +0530

    Create 13_largest_of3_numbers.py

---
+++ 13_largest_of3_numbers.py
@@ -0,0 +1,18 @@
+a=int(input("enter the value of a:")
+b=int(input("enter the value of b:")
+c=int(input("enter the value of c:")
+print(max(a,b,c))
+
+
+#using if-elif-else
+      
+a=int(input("enter the value of a:")
+b=int(input("enter the value of b:")
+c=int(input("enter the value of c:")
+if a>=b and b>=c:
+      print("a is greater")
+elif b>=c:
+      print("b is greater")
+else:
+      print("c is greater")
+
